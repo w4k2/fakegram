381af40e81480120d67d2132706932b650ca8769
Pawel Ksieniewicz <pawel@ksienie.com>
2020-09-08 16:09:34 +0200

    E1.

--- experiment.py
+++ experiment.py
@@ -2,22 +2,33 @@ import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from strlearn.metrics import balanced_accuracy_score
+from sklearn.utils import resample
 
 df = pd.read_csv('data_aAa.csv')
 corpus = df.values[:, 2].astype('U')
-y = df.values[:, -1]
-print(corpus)
-print(y)
+y = df.values[:, -1].astype(int)
+
+corpus, y = resample(corpus, y, random_state=1410, n_samples=1000)
 
 corpus_train, corpus_test, y_train, y_test = train_test_split(
     corpus, y, test_size=0.25, random_state=1410)
 
 # Extractor
-print(corpus_train)
 extractor = CountVectorizer().fit(corpus_train)
 
-print("Extracted")
+X_train = extractor.transform(corpus_train).toarray()
+X_test = extractor.transform(corpus_test).toarray()
 
-X_train = extractor.transform(corpus_train)
+# Classifier
+clf = MLPClassifier(random_state=1410)
 
-print(X_train, X_train.shape)
+print(X_train.shape, y_train.shape)
+clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test)
+
+print(y_pred)
+score = balanced_accuracy_score(y_test, y_pred)
+
+print(score)
