6e14cad1e9f5d9cf4298384e330ac6aac541bb32
Pawel Ksieniewicz <pawel@ksienie.com>
2020-09-08 16:14:11 +0200

    ABCD.

--- experiment.py
+++ experiment.py
@@ -6,29 +6,43 @@ from sklearn.neural_network import MLPClassifier
 from strlearn.metrics import balanced_accuracy_score
 from sklearn.utils import resample
 
-df = pd.read_csv('data_aAa.csv')
-corpus = df.values[:, 2].astype('U')
-y = df.values[:, -1].astype(int)
+df_a = pd.read_csv('data_aAa.csv')
+df_b = pd.read_csv('data.csv')
+corpus_a = df_a.values[:, 2].astype('U')
+corpus_b = df_b.values[:, 2].astype('U')
+y = df_a.values[:, -1].astype(int)
 
-corpus, y = resample(corpus, y, random_state=1410, n_samples=1000)
+corpus_a, corpus_b, y = resample(corpus_a, corpus_b, y,
+                                 random_state=1410, n_samples=1000)
 
-corpus_train, corpus_test, y_train, y_test = train_test_split(
-    corpus, y, test_size=0.25, random_state=1410)
+corpus_a_train, corpus_a_test, corpus_b_train, corpus_b_test, y_train, y_test = train_test_split(
+    corpus_a, corpus_b, y, test_size=0.25, random_state=1410)
 
+"""
+A
+"""
 # Extractor
-extractor = CountVectorizer().fit(corpus_train)
+extractor_a = CountVectorizer().fit(corpus_a_train)
+extractor_b = CountVectorizer().fit(corpus_b_train)
 
-X_train = extractor.transform(corpus_train).toarray()
-X_test = extractor.transform(corpus_test).toarray()
+X_train_a = extractor_a.transform(corpus_a_train).toarray()
+X_train_b = extractor_b.transform(corpus_b_train).toarray()
+X_test_a = extractor_a.transform(corpus_a_test).toarray()
+X_test_b = extractor_b.transform(corpus_b_test).toarray()
 
 # Classifier
-clf = MLPClassifier(random_state=1410)
+clf_a = MLPClassifier(random_state=1410)
+clf_b = MLPClassifier(random_state=1410)
 
-print(X_train.shape, y_train.shape)
-clf.fit(X_train, y_train)
-y_pred = clf.predict(X_test)
+print(X_train_a.shape, y_train.shape)
 
-print(y_pred)
-score = balanced_accuracy_score(y_test, y_pred)
+clf_a.fit(X_train_a, y_train)
+clf_b.fit(X_train_b, y_train)
 
-print(score)
+y_pred_a = clf_a.predict(X_test_a)
+y_pred_b = clf_b.predict(X_test_b)
+
+score_a = balanced_accuracy_score(y_test, y_pred_a)
+score_b = balanced_accuracy_score(y_test, y_pred_b)
+
+print(score_a, score_b)
