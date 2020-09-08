f93c10435a3473e64b921cb6d219cc71f978b269
Pawel Ksieniewicz <pawel@ksienie.com>
2020-09-08 17:13:12 +0200

    5x2 CV.

--- experiment_cv.py
+++ experiment_cv.py
@@ -8,59 +8,70 @@ from sklearn.utils import resample
 
 df_a = pd.read_csv('data_aAa.csv')
 df_b = pd.read_csv('data.csv')
-corpus_a = df_a.values[:, 2].astype('U')
-corpus_b = df_b.values[:, 2].astype('U')
-y = df_a.values[:, -1].astype(int)
+_corpus_a = df_a.values[:, 2].astype('U')
+_corpus_b = df_b.values[:, 2].astype('U')
+_y = df_a.values[:, -1].astype(int)
 
-corpus_a, corpus_b, y = resample(corpus_a, corpus_b, y,
-                                 random_state=1410, n_samples=1000)
+n_samples = _y.shape[0]
+n_splits = 2
+n_repeats = 5
 
-skf = StratifiedKFold(n_splits=5, shuffle=True)
+print("%i input samples" % n_samples)
 
 scores = []
 
-for fold, (train, test) in enumerate(skf.split(corpus_a, y)):
-    corpus_a_train = corpus_a[train]
-    corpus_b_train = corpus_b[train]
-    corpus_a_test = corpus_a[test]
-    corpus_b_test = corpus_b[test]
-    y_train = y[train]
-    y_test = y[test]
+for repeat in range(n_repeats):
+    corpus_a, corpus_b, y = resample(_corpus_a, _corpus_b, _y,
+                                     random_state=1410 + repeat,
+                                     n_samples=1000, stratify=_y,
+                                     replace=False)
 
-    # Extractor
-    extractor_a = CountVectorizer().fit(corpus_a_train)
-    extractor_b = CountVectorizer().fit(corpus_b_train)
+    skf = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=1410)
 
-    X_train_a = extractor_a.transform(corpus_a_train).toarray()
-    X_train_b = extractor_b.transform(corpus_b_train).toarray()
-    X_test_a = extractor_a.transform(corpus_a_test).toarray()
-    X_test_b = extractor_b.transform(corpus_b_test).toarray()
+    for fold, (train, test) in enumerate(skf.split(corpus_a, y)):
+        corpus_a_train = corpus_a[train]
+        corpus_b_train = corpus_b[train]
+        corpus_a_test = corpus_a[test]
+        corpus_b_test = corpus_b[test]
+        y_train = y[train]
+        y_test = y[test]
 
-    # Build classifiers
-    clf_a = MLPClassifier(random_state=1410)
-    clf_b = MLPClassifier(random_state=1410)
+        # Extractor
+        extractor_a = CountVectorizer(max_features=100,
+                                      ngram_range=(1, 2)).fit(corpus_a_train)
+        extractor_b = CountVectorizer(max_features=100,
+                                      ngram_range=(1, 1)).fit(corpus_b_train)
 
-    clf_a.fit(X_train_a, y_train)
-    clf_b.fit(X_train_b, y_train)
+        X_train_a = extractor_a.transform(corpus_a_train).toarray()
+        X_train_b = extractor_b.transform(corpus_b_train).toarray()
+        X_test_a = extractor_a.transform(corpus_a_test).toarray()
+        X_test_b = extractor_b.transform(corpus_b_test).toarray()
 
-    # Make ensemble
-    esm = np.array([clf_a.predict_proba(X_test_a),
-                    clf_b.predict_proba(X_test_b)])
-    f_esm = np.mean(esm, axis=0)
+        # Build classifiers
+        clf_a = MLPClassifier(random_state=1410)
+        clf_b = MLPClassifier(random_state=1410)
 
-    # Establish predictions
-    y_pred_a = clf_a.predict(X_test_a)
-    y_pred_b = clf_b.predict(X_test_b)
-    y_pred_c = np.argmax(f_esm, axis=1)
+        clf_a.fit(X_train_a, y_train)
+        clf_b.fit(X_train_b, y_train)
 
-    # Calculate scores
-    score_a = balanced_accuracy_score(y_test, y_pred_a)
-    score_b = balanced_accuracy_score(y_test, y_pred_b)
-    score_c = balanced_accuracy_score(y_test, y_pred_c)
+        # Make ensemble
+        esm = np.array([clf_a.predict_proba(X_test_a),
+                        clf_b.predict_proba(X_test_b)])
+        f_esm = np.mean(esm, axis=0)
 
-    print(fold, score_a, score_b, score_c)
+        # Establish predictions
+        y_pred_a = clf_a.predict(X_test_a)
+        y_pred_b = clf_b.predict(X_test_b)
+        y_pred_c = np.argmax(f_esm, axis=1)
 
-    scores.append([score_a, score_b, score_c])
+        # Calculate scores
+        score_a = balanced_accuracy_score(y_test, y_pred_a)
+        score_b = balanced_accuracy_score(y_test, y_pred_b)
+        score_c = balanced_accuracy_score(y_test, y_pred_c)
+
+        print(repeat, fold, score_a, score_b, score_c)
+
+        scores.append([score_a, score_b, score_c])
 
 scores = np.array(scores)
 
