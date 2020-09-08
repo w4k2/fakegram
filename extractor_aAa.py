cec65f4deaaa15d7b69a5facd92945f2a4a16268
Pawel Ksieniewicz <pawel@ksienie.com>
2020-09-08 15:40:48 +0200

    Data_aAa.

---
+++ extractor_aAa.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from string import ascii_lowercase, ascii_uppercase
+
+df = pd.read_csv('data.csv')
+
+df['title'] = df['title'].str.replace('o','a')
+
+for key in ['author', 'title', 'text']:
+    for i in ascii_lowercase:
+        df[key] = df[key].str.replace(i,'a')
+    for i in ascii_uppercase:
+        df[key] = df[key].str.replace(i,'A')
+
+print(df)
+
+df.to_csv('data_aaa.csv')
