4ac955d08b52f87890f22e1e63bb424ea9a4525f
Pawel Ksieniewicz <pawel@ksienie.com>
2020-09-08 15:58:41 +0200

    ABCD.

--- experiment.py
+++ experiment.py
@@ -1,17 +1,18 @@
 import pandas as pd
+import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 
 df = pd.read_csv('data_aAa.csv')
-titles = df.values[:, 2]
 y = df.values[:, -1]
-print(titles)
+print(corpus)
 print(y)
 
-titles_train, titles_test, y_train, y_test = train_test_split(
-    titles, y, test_size=0.25, random_state=1410)
+corpus_train, corpus_test, y_train, y_test = train_test_split(
+    corpus, y, test_size=0.25, random_state=1410)
 
 # Extractor
-extractor = CountVectorizer().fit(titles_train)
+print(corpus_train)
+#extractor = CountVectorizer().fit(corpus_train)
 
 print("Extracted")
