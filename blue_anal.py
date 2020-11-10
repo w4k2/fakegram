92ff70d451fb0b1676a1aca97818891f5a94c736
tibetansandfox <pawel.zyblewski@pwr.edu.pl>
2020-11-10 22:52:40 +0100

    Blue anal v.poor

--- blue_anal.py
+++ blue_anal.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.metrics import balanced_accuracy_score
+
 
 keys = ['text', 'author', 'title']
 i_s = ['words', 'struct']
@@ -36,18 +38,58 @@ print(n_to)
 probas = np.load("results/green_probas.npy", allow_pickle=True)
 y_true = np.load("results/green_ytest.npy", allow_pickle=True)
 
+
+# authors words + struct
+zbiornik_proba = np.zeros((2, 10), dtype=object)
+zbiornik_true = np.zeros((2, 10), dtype=object)
+
 best = 0
 for key_id, key in enumerate(keys):
     for extractor_id, i in enumerate(i_s):
+        fold = 0
         for repeat_id in range(5):
             for fold_id in range(2):
-                print(repeat_id, fold_id, q_id, key_id, extractor_id)
-                print(n_from[best], n_to[best])
+                if key == 'text':
+                # print(repeat_id, fold_id, q_id, key_id, extractor_id)
+                    print(n_from[best], n_to[best])
 
-                green_proba = probas[key_id, extractor_id, repeat_id, fold_id, q_id, n_from[best], n_to[best]]
-                y_test = y_true[key_id, extractor_id, repeat_id, fold_id, q_id, n_from[best], n_to[best]]
+                    green_proba = probas[key_id, extractor_id, repeat_id, fold_id, q_id, n_from[best], n_to[best]]
+                    y_test = y_true[key_id, extractor_id, repeat_id, fold_id, q_id, n_from[best], n_to[best]]
+                    zbiornik_proba[extractor_id, fold] = green_proba
+                    zbiornik_true[extractor_id, fold] = y_test
+                    fold+=1
+
                 # print(y_test)
                 # print(np.argmax(green_proba, axis=1))
                 # best_clf = np.argwhere(green_score==green_score.max())
                 # print("NAJLEPSZY:\n", best_clf)
         best+=1
+
+print(zbiornik_proba.shape)
+print(zbiornik_true.shape)
+
+probas_all = np.mean(zbiornik_proba, axis=0)
+probas_words = zbiornik_proba[0]
+probas_struct = zbiornik_proba[1]
+print(probas_words)
+scores_all = []
+scores_w = []
+scores_s = []
+# [print(np.argmax(probas[i], axis=1)) for i in range(probas.shape[0])]
+
+for i in range(probas_all.shape[0]):
+    pred = np.argmax(probas_all[i], axis=1)
+    pred_words = np.argmax(probas_words[i], axis=1)
+    pred_struct = np.argmax(probas_struct[i], axis=1)
+    true = zbiornik_true[0, i]
+    score = balanced_accuracy_score(true, pred)
+    scores_all.append(score)
+    score_w = balanced_accuracy_score(true, pred_words)
+    scores_w.append(score_w)
+    score_s = balanced_accuracy_score(true, pred_struct)
+    scores_s.append(score_s)
+
+print("text:")
+print("Words: %.3f | %.3f" % (np.mean(scores_w), np.std(scores_w)))
+print("Struct: %.3f | %.3f" % (np.mean(scores_s), np.std(scores_s)))
+print("Ensemble: %.3f | %.3f" % (np.mean(scores_all), np.std(scores_all)))
