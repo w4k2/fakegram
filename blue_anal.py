97234c14fed62c3270573fe3d85ffbd49df16b65
tibetansandfox <pawel.zyblewski@pwr.edu.pl>
2020-11-11 15:49:05 +0100

    Now it's acceptable

--- blue_anal.py
+++ blue_anal.py
@@ -7,7 +7,7 @@ keys = ['text', 'author', 'title']
 i_s = ['words', 'struct']
 n_range = 6
 ranger = np.array(range(n_range)) + 1
-# quantities = np.array([.02, .05, .1, .25, .40])
+quantities = np.array([.02, .05, .1, .25, .40])
 
 
 # KEYS x EXTRACTOR x REPEATS x FOLDS x QUANTITIES x FROM x TO
@@ -22,80 +22,58 @@ Finding best ngram range
 """
 # KEYS x EXTRACTOR x QUANTITIES x FROM x TO
 scores = np.mean(scores, axis=(2,3))
+# N_TABLES x x REPEATS x FOLDS
 
-quantities = np.array([.02, .05, .1, .25, .40])
+# QUANTITIES x KEYS x EXTRACTOR
+best_probas = np.zeros((5, 3, 2), dtype=object)
+
 n_from = []
 n_to = []
 for q_id, quantity in enumerate(quantities):
     for key_id, key in enumerate(keys):
         for extractor_id, i in enumerate(i_s):
             # print(q_id, key_id, extractor_id)
-
             green_score = scores[key_id, extractor_id, q_id]
-            # print(green_score)
             best_clf = np.argwhere(green_score==green_score.max())
             # print("NAJLEPSZY:\n", best_clf)
             n_from.append(best_clf[0,0])
             n_to.append(best_clf[0,1])
+            best_probas[q_id, key_id, extractor_id] = probas[key_id, extractor_id, :, :, q_id, best_clf[0,0], best_clf[0,1]]
 
 """
 Plots by key
 """
-zbiornik_proba = np.zeros((2, 10), dtype=object)
-best = 0
-plot_scores = np.zeros((3, 3, 5))
-for q_id, quantity in enumerate(quantities):
-    # authors words + struct
-    zbiornik_proba = np.zeros((2, 10), dtype=object)
-    zbiornik_true = np.zeros((2, 10), dtype=object)
-    print("Quantity: %.2f" % quantity)
-    for key_id, key in enumerate(keys):
-        for extractor_id, i in enumerate(i_s):
-            fold = 0
-            for repeat_id in range(5):
-                for fold_id in range(2):
-                        green_proba = probas[key_id, extractor_id, repeat_id, fold_id, q_id, n_from[best], n_to[best]]
-                        y_test = y_true[repeat_id, fold_id]
-                        zbiornik_proba[extractor_id, fold] = green_proba
-                        fold+=1
-            best+=1
-        probas_all = np.mean(zbiornik_proba, axis=0)
-        probas_words = zbiornik_proba[0]
-        probas_struct = zbiornik_proba[1]
-        scores_all = []
-        scores_w = []
-        scores_s = []
-
-        for i in range(probas_all.shape[0]):
-            pred = np.argmax(probas_all[i], axis=1)
-            pred_words = np.argmax(probas_words[i], axis=1)
-            pred_struct = np.argmax(probas_struct[i], axis=1)
-            true = y_true[i ,q_id]
-            score = balanced_accuracy_score(true, pred)
-            scores_all.append(score)
-            score_w = balanced_accuracy_score(true, pred_words)
-            scores_w.append(score_w)
-            score_s = balanced_accuracy_score(true, pred_struct)
-            scores_s.append(score_s)
-
-        print("%s:" % key)
-        print("Words: %.3f | %.3f" % (np.mean(scores_w), np.std(scores_w)))
-        print("Struct: %.3f | %.3f" % (np.mean(scores_s), np.std(scores_s)))
-        print("Ensemble: %.3f | %.3f" % (np.mean(scores_all), np.std(scores_all)))
-
-        plot_scores[key_id, 0, q_id] = np.mean(scores_w)
-        plot_scores[key_id, 1, q_id] = np.mean(scores_s)
-        plot_scores[key_id, 2, q_id] = np.mean(scores_all)
-print(plot_scores)
-
-
-labels = ['words', 'struct', 'ensemble']
-ls = [":", "--", "-"]
+y_true = np.load("results/green_ytest.npy", allow_pickle=True)
 for key_id, key in enumerate(keys):
-    scores = plot_scores[key_id]
+    print(key)
+    plot_scores = []
+    for q_id, quantity in enumerate(quantities):
+        y_test = y_true[:, :, q_id].reshape(10,)
+        separate_proba = best_probas[q_id, key_id, :]
+        w = separate_proba[0].reshape(10,)
+        s = separate_proba[1].reshape(10,)
+        e = np.mean(best_probas[q_id, key_id, :], axis=0).reshape(10,)
+
+        # calculate mean scores from folds
+        fold_scores = np.zeros((3 ,10))
+        for i in range(w.shape[0]):
+            pred_w = np.argmax(w[i], axis=1)
+            pred_s = np.argmax(s[i], axis=1)
+            pred_e = np.argmax(e[i], axis=1)
+            fold_scores[0,i] = balanced_accuracy_score(y_test[i], pred_w)
+            fold_scores[1,i] = balanced_accuracy_score(y_test[i], pred_s)
+            fold_scores[2,i] = balanced_accuracy_score(y_test[i], pred_e)
+        # print(np.mean(fold_scores, axis=1))
+        plot_scores.append(np.mean(fold_scores, axis=1))
+        # exit()
+    plot_scores = np.array(plot_scores).T
+
+    # Lets make some plots
+    labels = ['words', 'struct', 'ensemble']
+    ls = [":", "--", "-"]
     fig = plt.figure(figsize=(10, 5))
-    for i in range(scores.shape[0]):
-        plt.plot(scores[i], color='blue', ls=ls[i], label=labels[i])
+    for i in range(plot_scores.shape[0]):
+        plt.plot(plot_scores[i], color='blue', ls=ls[i], label=labels[i])
 
     plt.ylim(0.5, 1.0)
     plt.xticks([i for i in range(quantities.shape[0])], [str(type) for type in quantities])
@@ -103,55 +81,4 @@ for key_id, key in enumerate(keys):
     plt.title(key)
     plt.legend()
     plt.tight_layout()
-    plt.savefig("figures/%s_lineplot.png" % key, dpi=200)
-
-
-
-# best = 0
-# for key_id, key in enumerate(keys):
-#     for extractor_id, i in enumerate(i_s):
-#         fold = 0
-#         for repeat_id in range(5):
-#             for fold_id in range(2):
-#                 if key == 'text':
-#                 # print(repeat_id, fold_id, q_id, key_id, extractor_id)
-#                     # print(n_from[best], n_to[best])
-#
-#                     green_proba = probas[key_id, extractor_id, repeat_id, fold_id, q_id, n_from[best], n_to[best]]
-#                     y_test = y_true[key_id, extractor_id, repeat_id, fold_id, q_id, n_from[best], n_to[best]]
-#                     zbiornik_proba[extractor_id, fold] = green_proba
-#                     zbiornik_true[extractor_id, fold] = y_test
-#                     fold+=1
-#
-#                 # print(y_test)
-#                 # print(np.argmax(green_proba, axis=1))
-#                 # best_clf = np.argwhere(green_score==green_score.max())
-#                 # print("NAJLEPSZY:\n", best_clf)
-#         best+=1
-#
-#
-# probas_all = np.mean(zbiornik_proba, axis=0)
-# probas_words = zbiornik_proba[0]
-# probas_struct = zbiornik_proba[1]
-# # print(probas_words)
-# scores_all = []
-# scores_w = []
-# scores_s = []
-# # [print(np.argmax(probas[i], axis=1)) for i in range(probas.shape[0])]
-#
-# for i in range(probas_all.shape[0]):
-#     pred = np.argmax(probas_all[i], axis=1)
-#     pred_words = np.argmax(probas_words[i], axis=1)
-#     pred_struct = np.argmax(probas_struct[i], axis=1)
-#     true = zbiornik_true[0, i]
-#     score = balanced_accuracy_score(true, pred)
-#     scores_all.append(score)
-#     score_w = balanced_accuracy_score(true, pred_words)
-#     scores_w.append(score_w)
-#     score_s = balanced_accuracy_score(true, pred_struct)
-#     scores_s.append(score_s)
-#
-# print("text:")
-# print("Words: %.3f | %.3f" % (np.mean(scores_w), np.std(scores_w)))
-# print("Struct: %.3f | %.3f" % (np.mean(scores_s), np.std(scores_s)))
-# print("Ensemble: %.3f | %.3f" % (np.mean(scores_all), np.std(scores_all)))
+    plt.savefig("figures/%s_blueplot.png" % key, dpi=200)
