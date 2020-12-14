d0a5e15347ddebc6b29781d2028d59e2528333eb
unknown <226327@student.pwr.edu.pl>
2020-12-14 18:59:23 +0100

    redplots for struct

--- red_plots.py
+++ red_plots.py
@@ -25,74 +25,104 @@ probas = np.mean(probas, axis=(5,6))
 # Plots by keys - only for words
 for key_id, key in enumerate(keys):
     print("### Key: %s" % key)
-    plot_scores = []
-    for q_id, quantity in enumerate(quantities):
-        print("## Quantity: %s" % quantity)
-        y_test = y_true[:, :, q_id].reshape(10,)
-        words_probas = probas[key_id, 0, :, :, q_id].reshape(10,)
+    for extractor_id, extractor in enumerate(i_s):
+        print("## Extractors: %s" % extractor)
+        plot_scores = []
+        for q_id, quantity in enumerate(quantities):
+            print("# Quantity: %s" % quantity)
+            y_test = y_true[:, :, q_id].reshape(10,)
+            words_probas = probas[key_id, extractor_id, :, :, q_id].reshape(10,)
 
-        fold_scores = np.zeros(10)
-        for i in range(10):
-            pred_w = np.argmax(words_probas[i], axis=1)
-            fold_scores[i] = balanced_accuracy_score(y_test[i], pred_w)
-        plot_scores.append(np.mean(fold_scores))
-    plot_scores = np.array(plot_scores).T
+            fold_scores = np.zeros(10)
+            for i in range(10):
+                pred_w = np.argmax(words_probas[i], axis=1)
+                fold_scores[i] = balanced_accuracy_score(y_test[i], pred_w)
+            plot_scores.append(np.mean(fold_scores))
+        plot_scores = np.array(plot_scores).T
 
-    fig = plt.figure(figsize=(10, 5))
-    plt.plot(plot_scores, color='red', label='words')
+        fig = plt.figure(figsize=(10, 5))
+        plt.plot(plot_scores, color='red', label='words')
 
-    # Annotate
-    xytext = [(0,-10), (0,10), (0,10)]
-    for j in range(plot_scores.shape[0]):
-        plt.annotate(str(round(plot_scores[j], 3)), (j,plot_scores[j]), textcoords="offset points", xytext=xytext[0], ha='center', fontsize=6)
+        # Annotate
+        xytext = [(0,-10), (0,10), (0,10)]
+        for j in range(plot_scores.shape[0]):
+            plt.annotate(str(round(plot_scores[j], 3)), (j,plot_scores[j]), textcoords="offset points", xytext=xytext[0], ha='center', fontsize=6)
 
-    plt.ylim(0.5, 1.0)
-    plt.xticks([i for i in range(quantities.shape[0])], [str(type) for type in quantities])
-    plt.title("%s words" % key)
-    plt.legend()
-    plt.grid(ls="--", color=(0.85, 0.85, 0.85))
-    plt.tight_layout()
-    plt.savefig("figures/%s_redplot.png" % key, dpi=200)
+        plt.ylim(0.5, 1.0)
+        plt.xticks([i for i in range(quantities.shape[0])], [str(type) for type in quantities])
+        plt.title("%s %s" % (key, extractor))
+        plt.legend()
+        plt.grid(ls="--", color=(0.85, 0.85, 0.85))
+        plt.tight_layout()
+        plt.savefig("figures/%s_%s_redplot.png" % (key, extractor), dpi=200)
 
 
-# Plot of words
+# Plot of words and structs
 y_true = np.load("results/green_ytest.npy", allow_pickle=True)
 plot_scores = []
 for q_id, quantity in enumerate(quantities):
     print("## Quantity: %s" % quantity)
     y_test = y_true[:, :, q_id].reshape(10,)
-    txt = probas[0, 0, :, :, q_id].reshape(10,)
-    aut = probas[1, 0, :, :, q_id].reshape(10,)
-    ttl = probas[2, 0, :, :, q_id].reshape(10,)
-    e = np.mean(probas[:, 0, :, :, q_id], axis=0).reshape(10,)
+    txt_words = probas[0, 0, :, :, q_id].reshape(10,)
+    aut_words = probas[1, 0, :, :, q_id].reshape(10,)
+    ttl_words = probas[2, 0, :, :, q_id].reshape(10,)
+    e_words = np.mean(probas[:, 0, :, :, q_id], axis=0).reshape(10,)
 
-    fold_scores=np.zeros((4,10))
+    txt_struct = probas[0, 1, :, :, q_id].reshape(10,)
+    aut_struct = probas[1, 1, :, :, q_id].reshape(10,)
+    ttl_struct = probas[2, 1, :, :, q_id].reshape(10,)
+    e_struct = np.mean(probas[:, 1, :, :, q_id], axis=0).reshape(10,)
+
+    e = np.mean(probas[:, :, :, :, q_id], axis=(0,1)).reshape(10,)
+
+    fold_scores=np.zeros((9,10))
     for i in range(10):
-        pred_txt = np.argmax(txt[i], axis=1)
-        pred_aut = np.argmax(aut[i], axis=1)
-        pred_ttl = np.argmax(ttl[i], axis=1)
+        pred_txt_words = np.argmax(txt_words[i], axis=1)
+        pred_aut_words = np.argmax(aut_words[i], axis=1)
+        pred_ttl_words = np.argmax(ttl_words[i], axis=1)
+        pred_e_words = np.argmax(e_words[i], axis=1)
+
+        pred_txt_struct = np.argmax(txt_struct[i], axis=1)
+        pred_aut_struct = np.argmax(aut_struct[i], axis=1)
+        pred_ttl_struct = np.argmax(ttl_struct[i], axis=1)
+        pred_e_struct = np.argmax(e_struct[i], axis=1)
+
         pred_e = np.argmax(e[i], axis=1)
-        fold_scores[0,i] = balanced_accuracy_score(y_test[i], pred_txt)
-        fold_scores[1,i] = balanced_accuracy_score(y_test[i], pred_aut)
-        fold_scores[2,i] = balanced_accuracy_score(y_test[i], pred_ttl)
-        fold_scores[3,i] = balanced_accuracy_score(y_test[i], pred_e)
+
+        fold_scores[0,i] = balanced_accuracy_score(y_test[i], pred_txt_words)
+        fold_scores[1,i] = balanced_accuracy_score(y_test[i], pred_aut_words)
+        fold_scores[2,i] = balanced_accuracy_score(y_test[i], pred_ttl_words)
+        fold_scores[3,i] = balanced_accuracy_score(y_test[i], pred_e_words)
+
+        fold_scores[4,i] = balanced_accuracy_score(y_test[i], pred_txt_struct)
+        fold_scores[5,i] = balanced_accuracy_score(y_test[i], pred_aut_struct)
+        fold_scores[6,i] = balanced_accuracy_score(y_test[i], pred_ttl_struct)
+        fold_scores[7,i] = balanced_accuracy_score(y_test[i], pred_e_struct)
+
+        fold_scores[8,i] = balanced_accuracy_score(y_test[i], pred_e)
     plot_scores.append(np.mean(fold_scores, axis=1))
 
 plot_scores = np.array(plot_scores).T
-labels = ['text', 'author', 'title', 'ensamble']
-ls = ["--", ":", "-.", "-"]
+labels = ['text_w', 'author_w', 'title_w', 'ensemble_w', 'text_s', 'author_s', 'title_s', 'ensemble_s', 'ensemble_all']
+ls = [":", "--", "-.", "-", ":", "--", "-.", "-", "-"]
+colors = ["blue", "blue", "blue", "blue", "red", "red", "red", "red", "black"]
 fig = plt.figure(figsize=(10, 5))
 for i in range(plot_scores.shape[0]):
-    plt.plot(plot_scores[i], color='red', ls=ls[i], label=labels[i])
+    plt.plot(plot_scores[i], color=colors[i], ls=ls[i], label=labels[i])
+
     # Annotate
-    xytext = [(0,5), (0,5), (0,5), (0,5)]
+    xytext = [(0,5), (0,5), (0,5), (0,5), (0,5), (0,5), (0,-10), (0,5), (0,10)]
     for j in range(plot_scores[i].shape[0]):
         plt.annotate(str(round(plot_scores[i][j], 3)), (j,plot_scores[i][j]), textcoords="offset points", xytext=xytext[i], ha='center', fontsize=6)
+#        plt.annotate((str(round(plot_scores[i][j], 3))+"+"+str(round(stds[i][j], 3))), (j,plot_scores[i][j]), textcoords="offset points", xytext=xytext[i], ha='center', fontsize=4)
+
 
 plt.ylim(0.5, 1.0)
-plt.title("Ensamble")
+plt.title("Ensamble of 6")
 plt.xticks([i for i in range(quantities.shape[0])], [str(type) for type in quantities])
+plt.title("Ensemble of 6")
 plt.legend()
 plt.grid(ls="--", color=(0.85, 0.85, 0.85))
 plt.tight_layout()
-plt.savefig("figures/ensamble_redplot.png", dpi=200)
+plt.savefig("figures/ensemble6_redplot.png", dpi=200)
+plt.savefig("figures/ensemble6_redplot.eps")
