7f7241175ecc7011c2002830c15009c6fed89149
unknown <226327@student.pwr.edu.pl>
2020-12-17 13:39:13 +0100

    orange plots

---
+++ orange_plots.py
@@ -0,0 +1,303 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import balanced_accuracy_score
+
+keys = ['text', 'author', 'title']
+i_s = ['words', 'struct']
+n_range = 6
+ranger = np.array(range(n_range)) + 1
+quantities = np.array([.02, .05, .1, .25, .40])
+
+# KEYS x EXTRACTOR x REPEATS x FOLDS x QUANTITIES x FROM x TO
+scores = np.load("results/green.npy")
+# KEYS x EXTRACTOR x REPEATS x FOLDS x QUANTITIES x FROM x TO
+probas = np.load("results/green_probas.npy", allow_pickle=True)
+# REPEATS x FOLDS x QUANTITIES
+y_true = np.load("results/green_ytest.npy", allow_pickle=True)
+
+# KEYS x EXTRACTOR x QUANTITIES
+scores = np.mean(scores, axis=(2,3))
+probas = np.mean(probas, axis=6)
+
+# Results by key for 1-6
+print("### 1-6 ###")
+for key_id, key in enumerate(keys):
+    print("### Key: %s" % key)
+    for extractor_id, extractor in enumerate(i_s):
+        print("## Extractors: %s" % extractor)
+        plot_scores = []
+        std_scores = []
+        for q_id, quantity in enumerate(quantities):
+            print("# Quantity: %s" % quantity)
+            y_test = y_true[:, :, q_id].reshape(10,)
+            probas_16 = probas[key_id, extractor_id, :, :, q_id, 0].reshape(10,)
+
+            fold_scores = np.zeros(10)
+            for i in range(10):
+                pred_probas_16 = np.argmax(probas_16[i], axis=1)
+                fold_scores[i] = balanced_accuracy_score(y_test[i], pred_probas_16)
+            plot_scores.append(np.mean(fold_scores))
+            std_scores.append(np.std(fold_scores))
+        plot_scores = np.array(plot_scores).T
+
+        fig = plt.figure(figsize=(10, 5))
+        plt.plot(plot_scores, color='orange', label=extractor)
+
+        xytext = [(0,-10), (0,10), (0,10)]
+        for j in range(plot_scores.shape[0]):
+            plt.annotate(str(round(plot_scores[j], 3)), (j,plot_scores[j]), textcoords="offset points", xytext=xytext[0], ha='center', fontsize=6)
+
+        plt.ylim(0.5, 1.0)
+        plt.xticks([i for i in range(quantities.shape[0])], [str(type) for type in quantities])
+        plt.title("%s %s" % (key, extractor))
+        plt.legend()
+        plt.grid(ls="--", color=(0.85, 0.85, 0.85))
+        plt.tight_layout()
+        plt.savefig("figures/%s_%s_16_orangeplot.png" % (key, extractor), dpi=200)
+
+# Ensemble 1-6
+plot_scores = []
+for q_id, quantity in enumerate(quantities):
+    y_test = y_true[:, :, q_id].reshape(10,)
+
+    txt_words_16 = probas[0, 0, :, :, q_id, 0].reshape(10,)
+    aut_words_16 = probas[1, 0, :, :, q_id, 0].reshape(10,)
+    ttl_words_16 = probas[2, 0, :, :, q_id, 0].reshape(10,)
+    e_words_16 = np.mean(probas[:, 0, :, :, q_id, 0], axis=0).reshape(10,)
+
+    txt_struct_16 = probas[0, 1, :, :, q_id, 0].reshape(10,)
+    aut_struct_16 = probas[1, 1, :, :, q_id, 0].reshape(10,)
+    ttl_struct_16 = probas[2, 1, :, :, q_id, 0].reshape(10,)
+    e_struct_16 = np.mean(probas[:, 1, :, :, q_id, 0], axis=0).reshape(10,)
+
+    e_16 = np.mean(probas[:, :, :, :, q_id, 0], axis=(0,1)).reshape(10,)
+
+    fold_scores=np.zeros((9,10))
+    for i in range(10):
+        pred_txt_words_16 = np.argmax(txt_words_16[i], axis=1)
+        pred_aut_words_16 = np.argmax(aut_words_16[i], axis=1)
+        pred_ttl_words_16 = np.argmax(ttl_words_16[i], axis=1)
+        pred_e_words_16 = np.argmax(e_words_16[i], axis=1)
+
+        pred_txt_struct_16 = np.argmax(txt_struct_16[i], axis=1)
+        pred_aut_struct_16 = np.argmax(aut_struct_16[i], axis=1)
+        pred_ttl_struct_16 = np.argmax(ttl_struct_16[i], axis=1)
+        pred_e_struct_16 = np.argmax(e_struct_16[i], axis=1)
+
+        pred_e_16 = np.argmax(e_16[i], axis=1)
+
+        fold_scores[0,i] = balanced_accuracy_score(y_test[i], pred_txt_words_16)
+        fold_scores[1,i] = balanced_accuracy_score(y_test[i], pred_aut_words_16)
+        fold_scores[2,i] = balanced_accuracy_score(y_test[i], pred_ttl_words_16)
+        fold_scores[3,i] = balanced_accuracy_score(y_test[i], pred_e_words_16)
+
+        fold_scores[4,i] = balanced_accuracy_score(y_test[i], pred_txt_struct_16)
+        fold_scores[5,i] = balanced_accuracy_score(y_test[i], pred_aut_struct_16)
+        fold_scores[6,i] = balanced_accuracy_score(y_test[i], pred_ttl_struct_16)
+        fold_scores[7,i] = balanced_accuracy_score(y_test[i], pred_e_struct_16)
+
+        fold_scores[8,i] = balanced_accuracy_score(y_test[i], pred_e_16)
+    plot_scores.append(np.mean(fold_scores, axis=1))
+
+plot_scores = np.array(plot_scores).T
+labels = ['text_w', 'author_w', 'title_w', 'ensemble_w', 'text_s', 'author_s', 'title_s', 'ensemble_s', 'ensemble_all']
+ls = [":", "--", "-.", "-", ":", "--", "-.", "-", "-"]
+colors = ["blue", "blue", "blue", "blue", "red", "red", "red", "red", "black"]
+fig = plt.figure(figsize=(10, 5))
+for i in range(plot_scores.shape[0]):
+    plt.plot(plot_scores[i], color=colors[i], ls=ls[i], label=labels[i])
+
+    # Annotate
+    xytext = [(0,5), (0,5), (0,5), (0,5), (0,5), (0,5), (0,-10), (0,5), (0,10)]
+    for j in range(plot_scores[i].shape[0]):
+        plt.annotate(str(round(plot_scores[i][j], 3)), (j,plot_scores[i][j]), textcoords="offset points", xytext=xytext[i], ha='center', fontsize=6)
+
+plt.ylim(0.5, 1.0)
+plt.title("Ensamble of 6")
+plt.xticks([i for i in range(quantities.shape[0])], [str(type) for type in quantities])
+plt.title("Ensemble of 6")
+plt.legend()
+plt.grid(ls="--", color=(0.85, 0.85, 0.85))
+plt.tight_layout()
+plt.savefig("figures/ensemble6_16_orangeplot.png", dpi=200)
+plt.savefig("figures/ensemble6_16_orangeplot.eps")
+
+
+# Results by key for diagonal - struct and words
+probas = np.load("results/green_probas.npy", allow_pickle=True)
+print("### Diagonal ###")
+for key_id, key in enumerate(keys):
+    print("### Key: %s" % key)
+    for extractor_id, extractor in enumerate(i_s):
+        print("## Extractors: %s" % extractor)
+        plot_scores = []
+        std_scores = []
+        for q_id, quantity in enumerate(quantities):
+            print("# Quantity: %s" % quantity)
+            y_test = y_true[:, :, q_id].reshape(10,)
+
+            probas_diagonal = np.zeros(n_range, dtype=object)
+            for i in range(n_range):
+                probas_diagonal[i] = probas[key_id, extractor_id, :, :, q_id, i, i]
+            probas_diagonal = np.mean(probas_diagonal).reshape(10,)
+
+            fold_scores = np.zeros(10)
+            for i in range(10):
+                pred_probas_diagonal = np.argmax(probas_diagonal[i], axis=1)
+                fold_scores[i] = balanced_accuracy_score(y_test[i], pred_probas_diagonal)
+            plot_scores.append(np.mean(fold_scores))
+            std_scores.append(np.std(fold_scores))
+        plot_scores = np.array(plot_scores).T
+
+        fig = plt.figure(figsize=(10, 5))
+        plt.plot(plot_scores, color='orange', label=extractor)
+
+        # Annotate
+        xytext = [(0,-10), (0,10), (0,10)]
+        for j in range(plot_scores.shape[0]):
+            plt.annotate(str(round(plot_scores[j], 3)), (j,plot_scores[j]), textcoords="offset points", xytext=xytext[0], ha='center', fontsize=6)
+
+        plt.ylim(0.5, 1.0)
+        plt.xticks([i for i in range(quantities.shape[0])], [str(type) for type in quantities])
+        plt.title("%s %s" % (key, extractor))
+        plt.legend()
+        plt.grid(ls="--", color=(0.85, 0.85, 0.85))
+        plt.tight_layout()
+        plt.savefig("figures/%s_%s_diagonal_orangeplot.png" % (key, extractor), dpi=200)
+
+# Ensemble diagonal - plot
+plot_scores = []
+for q_id, quantity in enumerate(quantities):
+    y_test = y_true[:, :, q_id].reshape(10,)
+
+    txt_diagonal_words = np.zeros(n_range, dtype=object)
+    aut_diagonal_words = np.zeros(n_range, dtype=object)
+    ttl_diagonal_words = np.zeros(n_range, dtype=object)
+    e_diagonal_words = np.mean(probas[:, 0, :, :, q_id, :, :], axis=0)
+    e_diagonal_words_array = np.zeros(n_range, dtype=object)
+
+    txt_diagonal_struct = np.zeros(n_range, dtype=object)
+    aut_diagonal_struct = np.zeros(n_range, dtype=object)
+    ttl_diagonal_struct = np.zeros(n_range, dtype=object)
+    e_diagonal_struct = np.mean(probas[:, 1, :, :, q_id, :, :], axis=0)
+    e_diagonal_struct_array = np.zeros(n_range, dtype=object)
+
+    e_diagonal_ensemble = np.mean(probas[:, :, :, :, q_id, :, :], axis=(0,1))
+    e_diagonal_ensemble_array = np.zeros(n_range, dtype=object)
+
+    for i in range(n_range):
+        txt_diagonal_words[i] = probas[0, 0, :, :, q_id, i, i]
+        aut_diagonal_words[i] = probas[1, 0, :, :, q_id, i, i]
+        ttl_diagonal_words[i] = probas[2, 0, :, :, q_id, i, i]
+        e_diagonal_words_array[i] = e_diagonal_words[:, :, i, i]
+
+        txt_diagonal_struct[i] = probas[0, 1, :, :, q_id, i, i]
+        aut_diagonal_struct[i] = probas[1, 1, :, :, q_id, i, i]
+        ttl_diagonal_struct[i] = probas[2, 1, :, :, q_id, i, i]
+        e_diagonal_struct_array[i] = e_diagonal_struct[:, :, i, i]
+
+        e_diagonal_ensemble_array[i] = e_diagonal_ensemble[:, :, i, i]
+
+    txt_diagonal_words = np.mean(txt_diagonal_words).reshape(10,)
+    aut_diagonal_words = np.mean(aut_diagonal_words).reshape(10,)
+    ttl_diagonal_words = np.mean(ttl_diagonal_words).reshape(10,)
+    e_diagonal_words = np.mean(e_diagonal_words_array).reshape(10,)
+
+    txt_diagonal_struct = np.mean(txt_diagonal_struct).reshape(10,)
+    aut_diagonal_struct = np.mean(aut_diagonal_struct).reshape(10,)
+    ttl_diagonal_struct = np.mean(ttl_diagonal_struct).reshape(10,)
+    e_diagonal_struct = np.mean(e_diagonal_struct_array).reshape(10,)
+
+    e_diagonal = np.mean(e_diagonal_ensemble_array).reshape(10,)
+
+    fold_scores = np.zeros((9,10))
+    for i in range(10):
+        pred_txt_diagonal_words = np.argmax(txt_diagonal_words[i], axis=1)
+        pred_aut_diagonal_words = np.argmax(aut_diagonal_words[i], axis=1)
+        pred_ttl_diagonal_words = np.argmax(ttl_diagonal_words[i], axis=1)
+        pred_e_diagonal_words = np.argmax(e_diagonal_words[i], axis=1)
+
+        pred_txt_diagonal_struct = np.argmax(txt_diagonal_struct[i], axis=1)
+        pred_aut_diagonal_struct = np.argmax(aut_diagonal_struct[i], axis=1)
+        pred_ttl_diagonal_struct = np.argmax(ttl_diagonal_struct[i], axis=1)
+        pred_e_diagonal_struct = np.argmax(e_diagonal_struct[i], axis=1)
+
+        pred_e_diagonal = np.argmax(e_diagonal[i], axis=1)
+
+        fold_scores[0,i] = balanced_accuracy_score(y_test[i], pred_txt_diagonal_words)
+        fold_scores[1,i] = balanced_accuracy_score(y_test[i], pred_aut_diagonal_words)
+        fold_scores[2,i] = balanced_accuracy_score(y_test[i], pred_ttl_diagonal_words)
+        fold_scores[3,i] = balanced_accuracy_score(y_test[i], pred_e_diagonal_words)
+
+        fold_scores[4,i] = balanced_accuracy_score(y_test[i], pred_txt_diagonal_struct)
+        fold_scores[5,i] = balanced_accuracy_score(y_test[i], pred_aut_diagonal_struct)
+        fold_scores[6,i] = balanced_accuracy_score(y_test[i], pred_ttl_diagonal_struct)
+        fold_scores[7,i] = balanced_accuracy_score(y_test[i], pred_e_diagonal_struct)
+
+        fold_scores[8,i] = balanced_accuracy_score(y_test[i], pred_e_diagonal)
+    plot_scores.append(np.mean(fold_scores, axis=1))
+
+plot_scores = np.array(plot_scores).T
+labels = ['text_w', 'author_w', 'title_w', 'ensemble_w', 'text_s', 'author_s', 'title_s', 'ensemble_s', 'ensemble_all']
+ls = [":", "--", "-.", "-", ":", "--", "-.", "-", "-"]
+colors = ["blue", "blue", "blue", "blue", "red", "red", "red", "red", "black"]
+fig = plt.figure(figsize=(10, 5))
+for i in range(plot_scores.shape[0]):
+    plt.plot(plot_scores[i], color=colors[i], ls=ls[i], label=labels[i])
+
+    # Annotate
+    xytext = [(0,5), (0,5), (0,5), (0,5), (0,5), (0,5), (0,-10), (0,5), (0,10)]
+    for j in range(plot_scores[i].shape[0]):
+        plt.annotate(str(round(plot_scores[i][j], 3)), (j,plot_scores[i][j]), textcoords="offset points", xytext=xytext[i], ha='center', fontsize=6)
+
+plt.ylim(0.5, 1.0)
+plt.title("Ensamble of 6")
+plt.xticks([i for i in range(quantities.shape[0])], [str(type) for type in quantities])
+plt.title("Ensemble of 6")
+plt.legend()
+plt.grid(ls="--", color=(0.85, 0.85, 0.85))
+plt.tight_layout()
+plt.savefig("figures/ensemble6_diagonal_orangeplot.png", dpi=200)
+plt.savefig("figures/ensemble6_diagonal_orangeplot.eps")
+
+# Ensemble diagonal - fold build
+# Zostawiam jak jest, żeby te fold_scores były nienaruszone
+e_diagonal_words = np.mean(probas[:, 0, :, :, 4, :, :], axis=0)
+e_diagonal_words_array = np.zeros(n_range, dtype=object)
+
+e_diagonal_struct = np.mean(probas[:, 1, :, :, 4, :, :], axis=0)
+e_diagonal_struct_array = np.zeros(n_range, dtype=object)
+
+e_diagonal_ensemble = np.mean(probas[:, :, :, :, 4, :, :], axis=(0,1))
+e_diagonal_ensemble_array = np.zeros(n_range, dtype=object)
+
+for i in range(n_range):
+    e_diagonal_words_array[i] = e_diagonal_words[:, :, i, i]
+    e_diagonal_struct_array[i] = e_diagonal_struct[:, :, i, i]
+    e_diagonal_ensemble_array[i] = e_diagonal_ensemble[:, :, i, i]
+
+e_diagonal_words = np.mean(e_diagonal_words_array).reshape(10,)
+e_diagonal_struct = np.mean(e_diagonal_struct_array).reshape(10,)
+e_diagonal_ensemble = np.mean(e_diagonal_ensemble_array).reshape(10,)
+
+fold_scores = np.zeros((3,10))
+for i in range(10):
+    pred_e_diagonal_words = np.argmax(e_diagonal_words[i], axis=1)
+    pred_e_diagonal_struct = np.argmax(e_diagonal_struct[i], axis=1)
+    pred_e_diagonal_ensemble = np.argmax(e_diagonal_ensemble[i], axis=1)
+
+    fold_scores[0,i] = balanced_accuracy_score(y_test[i], pred_e_diagonal_words)
+    fold_scores[1,i] = balanced_accuracy_score(y_test[i], pred_e_diagonal_struct)
+    fold_scores[2,i] = balanced_accuracy_score(y_test[i], pred_e_diagonal_ensemble)
+
+print("## Ensemble")
+print("Words scores: %s" % np.mean(fold_scores[0]))
+print("Struct scores: %s" % np.mean(fold_scores[1]))
+print("Ensemble scores: %s" % np.mean(fold_scores[2]))
+
+print(fold_scores.shape)
+# Zakomentowane, zeby nie popsuc
+# final_plots_scores = np.load("final_plots_scores.npy")
+# final_plots_scores = np.concatenate((final_plots_scores, fold_scores), axis=0)
+# np.save("final_plots_scores", final_plots_scores)
+# print(final_plots_scores.shape)
